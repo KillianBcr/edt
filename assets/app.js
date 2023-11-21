981c05967a0f9f5cdffbf859bb0163a82e7e6ed5
sami0002 <komlanvi---angelo.samina@etudiant.univ-reims.fr>
2023-11-21 13:01:38 +0100

    Fix bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
-import './bootstrap';
 import { registerReactControllerComponents } from '@symfony/ux-react';
 import './js/color-mode';
 import './styles/app.scss';
+import './bootstrap';
+import 'bootstrap';
+
 
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
